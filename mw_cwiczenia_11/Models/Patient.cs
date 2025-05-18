68f56741a2b651883eb578cabaad9e04f08eb9e4
matwasdev <matwasdev@gmail.com>
2025-05-18 22:45:24 +0200

    init

---
+++ mw_cwiczenia_11/Models/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mw_cwiczenia_11.Models;
+
+[Table("Patient")]
+public class Patient
+{
+    [Key]
+    public int IdPatient { get; set; }
+    
+    [MaxLength(100)]
+    public string FirstName { get; set; }
+    
+    [MaxLength(100)]
+    public string LastName { get; set; }
+    
+    [Column(TypeName = "date")]
+    public DateTime Birthdate { get; set; }
+    
+    public ICollection<Prescription> Prescriptions { get; set; }
+}
---
+++ mw_cwiczenia_11/Models/Prescription.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mw_cwiczenia_11.Models;
+
+[Table("Prescription")]
+public class Prescription
+{
+    [Key]
+    public int IdPrescription { get; set; }
+    
+    [Column(TypeName = "date")]
+    public DateTime Date { get; set; }
+
+    [Column(TypeName = "date")]
+    public DateTime DueDate { get; set; }
+    
+    [ForeignKey(nameof(Patient))]
+    public int IdPatient { get; set; }
+    
+    [ForeignKey(nameof(Doctor))]
+    public int IdDoctor { get; set; }
+    
+    
+    public Patient Patient { get; set; }
+    public Doctor Doctor { get; set; }
+    
+    public ICollection<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+}
